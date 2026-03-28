8c422a1facae3fc993da098e8919f9c18d21b1c4
Veronika <veronika9ds114@gmail.com>
2026-03-28 18:31:19 +0100

    fix image path and add products

--- garden-green/client/src/pages/Allproducts/index.jsx
+++ garden-green/client/src/pages/Allproducts/index.jsx
@@ -133,7 +133,7 @@ function AllProducts() {
         </div>
       </div>
       <div className={styles.productsDiv}>
-        {displayedProducts.map((product) => (
+        {products.map((product) => (
           <div className={styles.productCard} key={product.id}>
             <img
               className={styles.productImg}
